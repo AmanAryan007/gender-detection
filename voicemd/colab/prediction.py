4ff999999254425c839257e357a624eb42a1f7e9
Aman Kumar <amanaryan9234@gmail.com>
2023-11-13 02:23:58 +0530

    Update prediction.py

--- voicemd/colab/prediction.py
+++ voicemd/colab/prediction.py
@@ -9,8 +9,8 @@ re_arrange_files()
 for audio_file in os.listdir('./audio_files/'):
     if audio_file.endswith('wav') or audio_file.endswith('mp3'):
 
-        make_a_prediction('./audio_files/'+audio_file, config_filepath ='./voicemd/voicemd/config.yaml',
-                          best_model_path='./voicemd/model.pt')
+        make_a_prediction('./audio_files/'+audio_file, config_filepath ='./gender-detection/voicemd/config.yaml',
+                          best_model_path='./gender-detection/model.pt')
 
     else:
         print(f'{audio_file} seems to have the wrong extension. We only support .wav and .mp3 work at this time.\n')
